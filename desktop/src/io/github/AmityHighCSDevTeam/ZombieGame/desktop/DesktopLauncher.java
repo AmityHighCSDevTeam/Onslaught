59587b8aa5c47ec57c96f3bb22fb2945afe204a9
SergeySave <sergeysav.nn@gmail.com>
2014-10-23 18:34:22 -0400

    Added the libGDX part of the project

---
+++ desktop/src/io/github/AmityHighCSDevTeam/ZombieGame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package io.github.AmityHighCSDevTeam.ZombieGame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import io.github.AmityHighCSDevTeam.ZombieGame.ZombieGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ZombieGame(), config);
+	}
+}
