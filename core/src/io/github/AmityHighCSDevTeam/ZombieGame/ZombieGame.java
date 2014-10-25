23115687b7275eb9d6d14d499dbac8839c5f8644
SergeySave <sergeysav.nn@gmail.com>
2014-10-25 14:30:47 -0400

    Added Screens

--- core/src/io/github/AmityHighCSDevTeam/ZombieGame/ZombieGame.java
+++ core/src/io/github/AmityHighCSDevTeam/ZombieGame/ZombieGame.java
@@ -1,27 +1,47 @@
 package io.github.AmityHighCSDevTeam.ZombieGame;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import io.github.AmityHighCSDevTeam.ZombieGame.client.screen.LoadingScreen;
+import io.github.AmityHighCSDevTeam.ZombieGame.client.screen.MainMenu;
 
-public class ZombieGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+import com.badlogic.gdx.Game;
+
+public class ZombieGame extends Game {
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		setScreen(new LoadingScreen());
+		
+		new Thread(new Runnable() {			
+			@Override
+			public void run() {
+				//Do Loading
+				
+				try {
+					Thread.sleep(1000);//Loading Delay Test
+					setScreen(new MainMenu());
+				} catch (InterruptedException e) {}
+				
+			}
+		}).start();
 	}
-
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		super.render();
+	}
+	@Override
+	public void dispose() {
+		super.dispose();
+	}
+	@Override
+	public void pause() {
+		super.pause();
+	}	
+	@Override
+	public void resize(int width, int height) {
+		super.resize(width, height);
+	}	
+	@Override
+	public void resume() {
+		super.resume();
 	}
 }
