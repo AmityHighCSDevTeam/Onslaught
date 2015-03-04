a37cc1cc67a8b69184046f8f5e8f341faf5431c6
SergeySave <sergeysav.nn@gmail.com>
2015-03-04 16:23:02 -0500

    Fixed Loading Screen

--- core/src/org/amityregion5/ZombieGame/ZombieGame.java
+++ core/src/org/amityregion5/ZombieGame/ZombieGame.java
@@ -13,6 +13,7 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator.FreeTypeFontParameter;
+import com.badlogic.gdx.utils.async.ThreadUtils;
 
 /**
  * 
@@ -20,15 +21,15 @@ import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator.FreeTypeFont
  *
  */
 public class ZombieGame extends Game {
-	
+
 	public static ZombieGame instance; //The current game
-	
+
 	public BitmapFont buttonFont; //Font that buttons use
 	public Texture buttonTexture; //Texture that buttons use
 	public boolean isServer; //Is the current instance a server
 	public WeaponRegistry weaponRegistry; //The registry for the weapons
 	public int width, height; //The width and height of the screen
-	
+
 	/**
 	 * 
 	 * @param isServer is the game a server
@@ -37,65 +38,60 @@ public class ZombieGame extends Game {
 		instance = this; //Set the instance
 		this.isServer = isServer; //Set if it is a server
 	}
-	
+
 	@Override
 	public void create () {
 		Gdx.app.setLogLevel(Application.LOG_DEBUG); //Set the log level
 
 		width = Gdx.graphics.getWidth();
 		height = Gdx.graphics.getHeight();
-		
+
 		if (!isServer) {
 			setScreen(new LoadingScreen()); //Set the screen to a loading screen
 		}
-		
-		
-		//Thread for loading the game
-		Gdx.app.postRunnable(new Runnable() { 
-			@Override
-			public void run() {
-				
-				//The gamedata folder
-				FileHandle gameData = Gdx.files.local("ZombieGameData/GameData");
-				
-				//"Mod" loading list of mods
-				FileHandle[] plugins = gameData.list();
-				
-				//Create the weapon registry
-				weaponRegistry = new WeaponRegistry();
-				
-				//Create the plugin loader
-				PluginLoader loader = new PluginLoader();
-				//Load the plugins
-				loader.loadPlugin(plugins);
-				
-				//If it is a client
-				if (!isServer) {
-					//Load the texture for buttons
-					buttonTexture = new Texture(Gdx.files.internal("images/button.png"));
-					
-					//Create the font generator
-					FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("font/Calibri.ttf"));
-					
-					//Size 24 font
-					FreeTypeFontParameter parameter = new FreeTypeFontParameter();
-					parameter.size = 24;
-					
-					//Generate the font
+
+		new Thread(() -> {
+			//The gamedata folder
+			FileHandle gameData = Gdx.files.local("ZombieGameData/GameData");
+
+			//"Mod" loading list of mods
+			FileHandle[] plugins = gameData.list();
+
+			//Create the weapon registry
+			weaponRegistry = new WeaponRegistry();
+
+			//Create the plugin loader
+			PluginLoader loader = new PluginLoader();
+			//Load the plugins
+			loader.loadPlugin(plugins);
+
+			//If it is a client
+			if (!isServer) {
+				//Load the texture for buttons
+				Gdx.app.postRunnable(() -> buttonTexture = new Texture(Gdx.files.internal("images/button.png")));
+
+				//Create the font generator
+				FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("font/Calibri.ttf"));
+
+				//Size 24 font
+				FreeTypeFontParameter parameter = new FreeTypeFontParameter();
+				parameter.size = 24;
+
+				//Generate the font
+				Gdx.app.postRunnable(() -> {
 					buttonFont = generator.generateFont(parameter);
-					
+					//Make the font black
+					buttonFont.setColor(0, 0, 0, 1);	
 					//Get rid of generator
 					generator.dispose();
-					
-					//Make the font black
-					buttonFont.setColor(0, 0, 0, 1);
-					
-					//Go to main menu
-					setScreen(new MainMenu());
-				}
+				});
+
+				//Go to main menu
+				Gdx.app.postRunnable(() -> setScreen(new MainMenu()));
 			}
-		});
+		}).start();;
 	}
+
 	@Override
 	public void render () {
 		super.render();
@@ -105,7 +101,7 @@ public class ZombieGame extends Game {
 		super.dispose();
 		buttonFont.dispose(); //Get rid of all used memory
 		buttonTexture.dispose();
-		
+
 	}
 	@Override
 	public void pause() {
@@ -115,7 +111,7 @@ public class ZombieGame extends Game {
 	public void resize(int width, int height) {
 		//this.width = width; //Save screen size
 		//this.height = height;	
-		
+
 		super.resize(width, height);
 	}	
 	@Override
--- core/src/org/amityregion5/ZombieGame/client/screen/LoadingScreen.java
+++ core/src/org/amityregion5/ZombieGame/client/screen/LoadingScreen.java
@@ -1,5 +1,7 @@
 package org.amityregion5.ZombieGame.client.screen;
 
+import org.amityregion5.ZombieGame.ZombieGame;
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
@@ -30,7 +32,7 @@ public class LoadingScreen implements Screen {
 
 	@Override
 	public void render(float delta) {
-		Gdx.gl.glClearColor(0, 1, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		
 		batch.begin();
@@ -81,7 +83,7 @@ public class LoadingScreen implements Screen {
 	}
 	@Override
 	public void dispose() {
-		//batch.dispose();
-		//calibri30.dispose();
+		batch.dispose();
+		calibri30.dispose();
 	}
 }
