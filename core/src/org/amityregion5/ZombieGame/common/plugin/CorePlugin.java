5a5c8f3741dca215cd873fc1fed678240de60063
SergeySave <sergeysav.nn@gmail.com>
2016-06-01 14:44:25 -0400

    Fixes missing texture in lantern

--- core/src/org/amityregion5/ZombieGame/common/plugin/CorePlugin.java
+++ core/src/org/amityregion5/ZombieGame/common/plugin/CorePlugin.java
@@ -57,14 +57,14 @@ public class CorePlugin implements IPlugin {
 	public void load() {
 		//Register the lanterns
 		Placeable.registeredObjects.put("Lantern_0", (g, vector) -> {
-			LanternModel lantern = new LanternModel(new EntityLantern(), g, Color.WHITE.cpy().mul(1, 1, 1, 130f / 255), "Core/Entity/Lantern/0.png", "Lantern_0");
+			LanternModel lantern = new LanternModel(new EntityLantern(), g, Color.WHITE.cpy().mul(1, 1, 1, 130f / 255), "Core/Entity/Lantern/Lantern.png", "Lantern_0");
 			lantern.setLight(new PointLight(g.getLighting(), 200, lantern.getColor(), 3, vector.x, vector.y));
 			lantern.getEntity().setFriction(0.99f);
 			lantern.getEntity().setMass(10);
 			return lantern;
 		});
 		Placeable.registeredObjects.put("Lantern_1", (g, vector) -> {
-			LanternModel lantern = new LanternModel(new EntityLantern(), g, Color.WHITE.cpy().mul(1, 1, 1, 130f / 255), "Core/Entity/Lantern/1.png", "Lantern_1");
+			LanternModel lantern = new LanternModel(new EntityLantern(), g, Color.WHITE.cpy().mul(1, 1, 1, 130f / 255), "Core/Entity/Lantern/Lantern.png", "Lantern_1");
 			lantern.setLight(new PointLight(g.getLighting(), 200, lantern.getColor(), 6, vector.x, vector.y));
 			lantern.getEntity().setFriction(0.99f);
 			lantern.getEntity().setMass(10);
